bbbbb0be0d353b22e1f5a034a2625b7bb0dc3c6a
Deepika8888 <140224566+Deepika8888@users.noreply.github.com>
2024-06-26 16:43:06 +0545

    Add files via upload

---
+++ guessing_game.py
@@ -0,0 +1,23 @@
+Secret_word = "Deepika"
+guess = ""
+guess_count = 0
+guess_limit= 3
+out_of_guesses= False
+
+while guess != Secret_word and not (out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count +=1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses: 
+    print("Out of guesses, you loose")
+else: 
+    print("You win!")
+    
+
+
+
+
+      
---
+++ word_replacement.py
@@ -0,0 +1,11 @@
+
+def replaced_word():
+
+    str = "Hey, I am deepika."
+    word_to_replace= input("Enter the word to replace: ")
+    word_replacement = input("Enter the replacemnt: ")
+
+    print(str.replace(word_to_replace, word_replacement))
+
+
+replaced_word()
